93607906d2c6300d85ee29beeb7ef222610ebae4
blastking2510@gmail.com <blastking2510@gmail.com>
2023-09-17 10:09:35 -0700

    server.js

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
 const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-require('dotenv').config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const sequelize = new Sequelize(
-    process.env.ecommerce_db,
-    process.env.JacobG2510,
-    process.env.win,
-    {
-        host:process.env.JacobG2510,
-        dialect: 'mysql',
-    }
-);
+app.use(routes);
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Database connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-app.listen(PORT, () => {
-    console.log(`server is running oon port ${PORT}`)
-})
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
